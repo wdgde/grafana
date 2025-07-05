c0891676f597632cf1a796ae87bcca6885815033
Jack Westbrook <jack.westbrook@gmail.com>
2025-07-05 14:30:33 +0200

    build(frontend): replace publicPath with auto publicPath

--- packages/grafana-ui/src/components/Icon/utils.ts
+++ packages/grafana-ui/src/components/Icon/utils.ts
@@ -44,19 +44,6 @@ export function getSvgSize(size: IconSize) {
   }
 }
 
-let iconRoot: string | undefined;
-
 export function getIconRoot(): string {
-  if (iconRoot) {
-    return iconRoot;
-  }
-
-  const grafanaPublicPath = typeof window !== 'undefined' && window.__grafana_public_path__;
-  if (grafanaPublicPath) {
-    iconRoot = grafanaPublicPath + 'build/img/icons/';
-  } else {
-    iconRoot = 'public/build/img/icons/';
-  }
-
-  return iconRoot;
+  return __webpack_public_path__ + 'public/build/img/icons/';
 }
